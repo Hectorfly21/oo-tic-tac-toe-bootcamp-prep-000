1903c1a19182fa53b937cf38dc5f5f58fee2059d
Hector <hectorfly21@gmail.com>
2020-09-18 18:50:41 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -28,6 +28,6 @@ def move(index, current_player = "X")
   @board[index] = current_player
   end
 def position_taken?(index)
-  !(@board[index] = nil || @board[index] = " ")
+  !(@board[index] = nil? || @board[index] = " ")
   end
 end

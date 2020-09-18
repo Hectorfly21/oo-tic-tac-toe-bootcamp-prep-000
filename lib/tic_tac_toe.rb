d6a7edc3e7b19bb2175f766d8cb0dadc2572cc96
Hector <hectorfly21@gmail.com>
2020-09-18 18:59:56 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -31,6 +31,9 @@ def position_taken?(index)
   !(@board[index].nil? || @board[index] == " ")
   end
 def valid_move?(index)
-  index.between?(0,8) && !position_taken(index)
+  if index.between?(0,8) && !position_taken(index)
+  return true 
+else 
+  nil
   end
 end
